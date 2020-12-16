09bb18cf7dab862798a89903b17af665484c0f7f
ginnnnnn <aginlo@hotmail.com>
2020-12-17 00:53:49 +0800

    key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-export const KEY = 'AIzaSyAnxuxvTxbpnk-m55SZyuyG3xLr9Zg5voE';
+export const KEY = 'YOUR_API_KEY';
 
 export const youtubeSearch = axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3/',
